f36deebac7a4581c3f82d4bcb8f349856a52e228
djabirsadaoui <sadaoui.djabir@yahoo.com>
2020-10-26 10:40:12 +0100

    add a cache for the images

--- LeBoncoin/LeBoncoin/Extension/extension+UIImageView.swift
+++ LeBoncoin/LeBoncoin/Extension/extension+UIImageView.swift
@@ -7,23 +7,40 @@
 
 import UIKit
 
+class ImageStore: NSObject {
+    static let imageCache = NSCache<NSString, UIImage>()
+}
 extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit)  {
         contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
+        guard let url = URL(string: link) else {
+            return
+        }
+        let urlToString = url.absoluteString as NSString
+        
+        if let cachedImage = ImageStore.imageCache.object(forKey: urlToString) {
+            DispatchQueue.main.async {
+                self.image = cachedImage
             }
-        }.resume()
+        } else  {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard
+                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                    let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                    let data = data, error == nil,
+                    let image = UIImage(data: data)
+                else {
+                    DispatchQueue.main.async {
+                        self.image = nil
+                    }
+                    return
+                }
+                DispatchQueue.main.async() { [weak self] in
+                    ImageStore.imageCache.setObject(image, forKey: urlToString)
+                    self?.image = image
+                }
+            }.resume()
+        }
     }
-    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
-    }
+    
 }
