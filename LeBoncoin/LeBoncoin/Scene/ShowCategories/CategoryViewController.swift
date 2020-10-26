d780fd0c1a03fd6b606106d955f5e4e85d4d47a9
djabirsadaoui <sadaoui.djabir@yahoo.com>
2020-10-26 10:29:34 +0100

    set design for cetegoriesViewController

--- LeBoncoin/LeBoncoin/Scene/ShowCategories/CategoryViewController.swift
+++ LeBoncoin/LeBoncoin/Scene/ShowCategories/CategoryViewController.swift
@@ -9,37 +9,45 @@ import UIKit
 
 
 class CategoryViewController : UITableViewController {
-    
+    // MARK: Vars
     typealias SelectionHandler = (Category) -> Void
-
-    
     private var items : [Category]
-    func displayItems(items: [Category]) {
-        self.items = items
-        self.tableView.reloadData()
-    }
     private let onSelect : SelectionHandler?
     
+    //MARK: Initializer
     init(_ items : [Category], onSelect : SelectionHandler? = nil) {
         self.items = items
         self.onSelect = onSelect
         super.init(style: .plain)
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.backgroundColor = .thirdColor
+        self.tableView.separatorColor = .secondColor
+        self.tableView.separatorInset = UIEdgeInsets.zero
+    }
+    // MARK: Functions
+    func displayItems(items: [Category]) {
+        self.items = items
+        self.tableView.reloadData()
+    }
     
+    //MARK: Handling data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.backgroundColor = .thirdColor
+        cell.textLabel?.textColor = .white
         cell.textLabel?.text = items[indexPath.row].name
         return cell
     }
     
+    //MARK: Handling tableView delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.dismiss(animated: true)
         onSelect?(items[indexPath.row])
