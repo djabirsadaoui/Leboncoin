a188513ff524cd05ef5930a509f128efef237724
djabirsadaoui <sadaoui.djabir@yahoo.com>
2020-10-26 10:32:37 +0100

    code review and rename the file

--- LeBoncoin/LeBoncoin/Scene/Home/Views/ButtonWithImage.swift
+++ LeBoncoin/LeBoncoin/Scene/Home/Views/ButtonWithImage.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ButtonWithImage: UIView {
-
-    private let titleLabel = UILabel()
-    private let imageView = UIImageView()
-    private let tapButton = UITapGestureRecognizer()
+    // MARK: Vars
     var action: (() ->Void)? = nil
+    private let tapButton = UITapGestureRecognizer()
+    private let imageView: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondColor
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     var title: String = "" {
         didSet {
             titleLabel.text = title
@@ -23,40 +33,36 @@ class ButtonWithImage: UIView {
             imageView.image = image
         }
     }
-    
     var titleColor: UIColor = .black {
         didSet {
             titleLabel.textColor = titleColor
         }
     }
-   
+    
+    // MARK: Initializer
     override init(frame: CGRect) {
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.titleLabel.font = UIFont.boldSystemFont(ofSize: 13)
         super.init(frame: frame)
         self.backgroundColor = .clear
         self.addSubview(titleLabel)
         self.addSubview(imageView)
         self.tapButton.addTarget(self, action: #selector(handleTap(_:)))
         self.addGestureRecognizer(tapButton)
-    }
-    
-    override func layoutSubviews() {
+        // Add constraints
         imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5).isActive = true
         imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 2).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 5).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 10).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 7).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 20).isActive = true
         imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         titleLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-     @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+    
+    // MARK: Handing events
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
         if let action = self.action {
             action()
         }
