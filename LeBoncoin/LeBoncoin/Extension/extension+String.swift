536574ee23bad23d0334f809d948900ccd24542e
djabirsadaoui <sadaoui.djabir@yahoo.com>
2020-10-26 10:30:26 +0100

    add getDate to String extension

--- LeBoncoin/LeBoncoin/Extension/extension+String.swift
+++ LeBoncoin/LeBoncoin/Extension/extension+String.swift
@@ -9,9 +9,28 @@ import Foundation
 
 extension String {
     
+    /// This function converts a String to Date
+    /// - Returns: it returns the date which corresponds to the String
     func toDate() -> Date? {
         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.timeZone = TimeZone.current
         dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"
         let date = dateFormatter.date(from: self)
         return date
-    }}
+    }
+    
+    /// This function returns a date in the format passed in parameter
+    /// - Parameter format: date format
+    /// - Returns: it returns date in String format
+    func getDate(_ format: String = "d MMM y, HH:mm") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.dateFormat = format
+        guard let date = self.toDate() else {
+            return ""
+        }
+        return dateFormatter.string(from: date)
+    }
+}
